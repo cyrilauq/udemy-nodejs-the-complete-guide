ee7e994924d3a2cd1eab565cd969130f62e8b2a2
Auquier Cyril <cyrilauquier@hotmail.fr>
2023-09-07 13:28:09 +0200

    Lesson 221 - Working on the shopping cart

--- Section 13/00-starting-setup/models/user.js
+++ Section 13/00-starting-setup/models/user.js
@@ -26,4 +26,24 @@ const userSchema = new Schema({
     },
 });
 
+// We'll add new function to our schema with method properties, wich allows us to add function to our schema
+userSchema.methods.addToCart = function(product) {
+    const cartProductIndex = this.cart.items === undefined ? -1 : this.cart.items.findIndex(item => item.productId.toString() === product._id.toString());
+    let quantity = (cartProductIndex > -1 ? this.cart.items[cartProductIndex].quantity : 1);
+    const updatedCartItems = [...this.cart.items];
+    if(cartProductIndex > -1) {
+        updatedCartItems[cartProductIndex].quantity++;
+    } else {
+        updatedCartItems.push({
+            productId: product._id,
+            quantity
+        });
+    }
+    const updatedCart = { 
+        items: updatedCartItems
+    }
+    this.cart = updatedCart;
+    return this.save();
+}
+
 module.exports = mongoose.model('User', userSchema);
