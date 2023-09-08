a3ba8be639806ea30d5561a54c1f1c1cbfe7c0c3
Auquier Cyril <cyrilauquier@hotmail.fr>
2023-09-08 12:41:16 +0200

    Lesson 238 - Adding sessions

--- Section 14/00-starting-setup/app.js
+++ Section 14/00-starting-setup/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const User = require('./models/user');
 
@@ -19,6 +20,14 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// We'll initialize the session and pass a js object to configure the session
+// We can configure the session cookie by adding the "cookie" property to the object and give value to its properties too
+app.use(session({
+    secret: 'my secret', // Will be used to hache our data
+    reseave: false, // this means that the session will not be save on every request but only if something change in it
+    saveUninitialized: false, // This means that the session will not be save for a request that doesn't need it
+}));
 
 app.use((req, res, next) => {
     User.findById("64f9a4caa0b3495535d49017")
