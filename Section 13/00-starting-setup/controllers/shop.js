8311c8bc2714f353dd68eff7fcaf26417d531a17
Auquier Cyril <cyrilauquier@hotmail.fr>
2023-09-08 10:19:29 +0200

    Lessons 227 - Viewing orders

--- Section 13/00-starting-setup/controllers/shop.js
+++ Section 13/00-starting-setup/controllers/shop.js
@@ -114,14 +114,15 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+    Order.find({
+        "user._id": req.user._id
+    })
     .then(orders => {
-      res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your Orders',
-        orders: orders
-      });
+        res.render('shop/orders', {
+            path: '/orders',
+            pageTitle: 'Your Orders',
+            orders: orders
+        });
     })
     .catch(err => console.log(err));
 };
