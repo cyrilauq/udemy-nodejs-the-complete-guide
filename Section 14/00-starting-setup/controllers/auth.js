7fa95cb03764d1dd83a343381c365e3d44418922
Auquier Cyril <cyrilauquier@hotmail.fr>
2023-09-08 11:17:03 +0200

    Lesson 235 - Setting a cookie

--- Section 14/00-starting-setup/controllers/auth.js
+++ Section 14/00-starting-setup/controllers/auth.js
@@ -1,6 +1,16 @@
 exports.getLogin = (req, res, next) => {
+    const isLoggedIn = req.get('Cookie').split(';')[0].split('=')[1];
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
+        userAuthenticated: isLoggedIn
     });
+};
+
+exports.postLogin = (req, res, next) => {
+    // To set a cookie we'll set a header, by using setHeader
+    // It takes the header name's and it's value
+    // The Set-Cookie name, is a reserved name wich means it has already a default behavior, is used for setting cookies 
+    res.setHeader('Set-Cookie', 'userLoggedIn=true');
+    res.redirect('/');
 };
--- Section 14/00-starting-setup/routes/auth.js
+++ Section 14/00-starting-setup/routes/auth.js
@@ -5,4 +5,6 @@ const authController = require('../controllers/auth');
 
 routes.get('/login', authController.getLogin);
 
+routes.post('/login', authController.postLogin);
+
 module.exports = routes;
